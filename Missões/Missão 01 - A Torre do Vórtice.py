f8ef426e00a7d753d5dc0587ecff3c4fa8acfa4b
João Igor de Sousa Ferro <joaoigorsf990@gmail.com>
2026-05-19 00:04:13 -0300

    Refactor camera functions and remove unused code

--- Missões/Missão 01 - A Torre do Vórtice.py
+++ Missões/Missão 01 - A Torre do Vórtice.py
@@ -1,56 +1,41 @@
 import OpenGL.GL as gl
 import OpenGL.GLUT as glut
 import OpenGL.GLU as glu
-import random as rd
 import math
 import sys
 
-r = 0
-g = 0
-b = 0
 
 def keyboard(key, x, y):
    if key == b's':
-      cor()
       cam_top()
 
    elif key == b'a':
-      cor()
       cam_sideLeft()
 
    elif key == b'd':
-      cor()
       cam_sideRight()
 
    elif key == b'w':
-      cor()
-      cam_front()
+      cam_base()
 
    elif key == b'r':
-      cor()
-      cam_padrao()
+      cam_reset()
 
-def cor():
-  global r, g, b
-  r = rd.random()
-  g = rd.random()
-  b = rd.random()
-
-def cam_front():
-   gl.glClearColor(r, g, b, 1)
+def cam_top():
+   gl.glClearColor(0.5, 0, 0, 1)
    gl.glMatrixMode(gl.GL_PROJECTION)
    gl.glLoadIdentity()
-   glu.gluPerspective(64, 1.0, 1.0, 10)
+   glu.gluPerspective(32, 1.0, 1.0, 10)
    gl.glMatrixMode(gl.GL_MODELVIEW)
    gl.glLoadIdentity()
-   glu.gluLookAt(0.0, 0.0, 2.0,
+   glu.gluLookAt(0.0, 5.0, 2.0,
                  0.0, 0.0, 0.0,
                  0.0, 1.0, 0.0)
 
    glut.glutPostRedisplay()
 
 def cam_sideLeft():
-   gl.glClearColor(r, g, b, 1)
+   gl.glClearColor(0, 0.5, 0, 1)
    gl.glMatrixMode(gl.GL_PROJECTION)
    gl.glLoadIdentity()
    glu.gluPerspective(32, 1.0, 1.0, 10)
@@ -63,7 +48,7 @@ def cam_sideLeft():
    glut.glutPostRedisplay()
 
 def cam_sideRight():
-   gl.glClearColor(r, g, b, 1)
+   gl.glClearColor(0, 0, 0.5, 1)
    gl.glMatrixMode(gl.GL_PROJECTION)
    gl.glLoadIdentity()
    glu.gluPerspective(32, 1.0, 1.0, 10)
@@ -75,21 +60,21 @@ def cam_sideRight():
 
    glut.glutPostRedisplay()
 
-def cam_top():
-   gl.glClearColor(r, g, b, 1)
+def cam_base():
+   gl.glClearColor(1, 0.5, 0, 1)
    gl.glMatrixMode(gl.GL_PROJECTION)
    gl.glLoadIdentity()
-   glu.gluPerspective(32, 1.0, 1.0, 10)
+   glu.gluPerspective(64, 1.0, 1.0, 10)
    gl.glMatrixMode(gl.GL_MODELVIEW)
    gl.glLoadIdentity()
-   glu.gluLookAt(0.0, 5.0, 2.0,
+   glu.gluLookAt(0.0, 0.0, 2.0,
                  0.0, 0.0, 0.0,
                  0.0, 1.0, 0.0)
 
    glut.glutPostRedisplay()
 
-def cam_padrao(): 
-   gl.glClearColor(r, g, b, 1)
+def cam_reset(): 
+   gl.glClearColor(0, 0, 0, 1)
    gl.glMatrixMode(gl.GL_PROJECTION)
    gl.glLoadIdentity()
    glu.gluPerspective(32, 1.0, 1.0, 100)
@@ -102,7 +87,7 @@ def cam_padrao():
    glut.glutPostRedisplay()
 
 def cone():
-   gl.glColor3f(1 - r, 1 - g, 1 - b)
+   gl.glColor3f(1, 1, 1)
    glut.glutWireCone(1, 1, 25, 10)
 
 def draw():
@@ -116,7 +101,7 @@ glut.glutCreateWindow("A torre do Vórtice")
 
 gl.glEnable(gl.GL_DEPTH_TEST)
 
-cam_front()
+cam_reset()
 
 glut.glutReshapeWindow(800, 800)
 glut.glutDisplayFunc(draw)
